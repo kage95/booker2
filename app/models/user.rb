cb4dac4ac82de63c39e80a7a1db3340b7da864ec
kage <81172243+kage95@users.noreply.github.com>
2022-01-10 14:50:39 +0000

    [add]indexページのレイアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :books,dependent: :destroy
+  has_one_attached:profile_image
+  
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join("app/assets/images/no-image.jpeg")
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpeg', content_type: 'image/jpeg')
+    else
+      profile_image.variant(resize:"63x63").processed
+    end
+  end
 end
